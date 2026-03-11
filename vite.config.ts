a1a9cf90d7f8376d9c2c19faa5bda074d6e8773e
Hundefira Nassir <108434135+HundefraN@users.noreply.github.com>
2026-03-11 20:02:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/proposals_made_with_EDS_/',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
